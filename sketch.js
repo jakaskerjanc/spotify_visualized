694543c3057dc80dc8345f1f41ef1e98c3df348e
jakaskerjanc <100130505+jakaskerjanc@users.noreply.github.com>
2024-11-06 19:33:24 +0100

    Racing bar chart

--- sketch.js
+++ sketch.js
@@ -1,222 +1,189 @@
-let monthSlider, button
-let months
-let selectedMonth = '2018-12'
-let isAutoplayOn = true
+// Convert cumulativeListeningTime to the same format as data
+const data = _.flatten(Object.entries(cumulativeListeningTime).map(([date, artistMinuteMap]) => {
+    return Object.entries(artistMinuteMap).map(([artist, minutes]) => { return { date, name: artist, value: Math.round(minutes / 60000) } })
+}))
 
-let w
-let h
+const names = new Set(data.map(d => d.name))
 
-let graphXMargin
-let graphYMargin
-let graphWidth
-let graphHeight
-let barWidth
-let graphTopEdge
-const sliderMargin = 90
+const scale = d3.scaleOrdinal(d3.schemeTableau10);
 
-let top5Artists, artistColors
+const n = 12
+const k = 10
+const duration = 200
+const barSize = 48
+const marginTop = 16
+const marginRight = 6
+const marginBottom = 6
+const marginLeft = 0
+const height = marginTop + barSize * n + marginBottom
+const width = 960
 
-recalcDimensions()
+const x = d3.scaleLinear([0, 1], [marginLeft, width - marginRight])
 
-let cumulativeListeningData
+const y = d3.scaleBand()
+    .domain(d3.range(n + 1))
+    .rangeRound([marginTop, marginTop + barSize * (n + 1 + 0.1)])
+    .padding(0.1)
 
-let canvas
+const tickFormat = undefined
 
-function preload() {
-    cumulativeListeningData = loadJSON('data/cumulativeListeningTime.json')
-}
+const formatDate = d3.utcFormat("%Y-%m")
 
-function setup() {
-    top5Artists = calculateTop5Artists()
-    artistColors = getUniqueColorForEachArtists()
+d3.select('body').append('svg');
+const svg = d3.select("svg")
+    .attr("viewBox", [0, 0, width, height])
+    .attr("width", width)
+    .attr("height", height)
+    .attr("style", "max-width: 100%; height: auto;");
 
-    canvas = createCanvas(w, h)
+const datevalues = Array.from(d3.rollup(data, ([d]) => d.value, d => d.date, d => d.name))
+    .map(([date, data]) => [new Date(date), data])
+    .sort(([a], [b]) => d3.ascending(a, b))
 
-    sliderSetup()
-    buttonSetup()
-
-    dataContainer = d3.select('body').append('custom');
-    refresh();
-    setInterval(refresh, 1000);
-    setInterval(incrementSelectedMonth, 1000);
-    d3.timer(drawFunction);
-}
-
-function incrementSelectedMonth() {
-    if (!isAutoplayOn) {
-        return
+const keyframes = [];
+let ka, a, kb, b;
+for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
+    for (let i = 0; i < k; ++i) {
+        const t = i / k;
+        keyframes.push([
+            new Date(ka * (1 - t) + kb * t),
+            rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
+        ]);
     }
-    const monthIndex = months.indexOf(selectedMonth)
-    const nextMonthIndex = (monthIndex + 1) % months.length
-    selectedMonth = months[nextMonthIndex]
-    monthSlider.value(nextMonthIndex)
 }
+keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
 
-function getUniqueColorForEachArtists() {
-    const allArtists = new Set()
+const nameframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.name)
+const prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
+const next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
 
-    for (const month in top5Artists) {
-        top5Artists[month].forEach(([artist, minutes]) => allArtists.add(artist))
+const updateBars = bars(svg);
+const updateAxis = axis(svg);
+const updateLabels = labels(svg);
+const updateTicker = ticker(svg);
+
+const node = svg.node();
+
+async function render() {
+    for (const keyframe of keyframes) {
+        const transition = svg.transition()
+            .duration(duration)
+            .ease(d3.easeLinear);
+
+        // Extract the top bar’s value.
+        x.domain([0, keyframe[1][0].value]);
+
+        updateAxis(keyframe, transition);
+        updateBars(keyframe, transition);
+        updateLabels(keyframe, transition);
+        updateTicker(keyframe, transition);
+
+        // invalidation.then(() => svg.interrupt());
+        await transition.end();
     }
-
-    let shuffledArtists = Array.from(allArtists)
-        .map(value => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value)
-
-    const colors = {}
-    const hueStep = 360 / allArtists.size
-    let hue = 0
-
-    shuffledArtists.forEach(artist => {
-        colors[artist] = `hsl(${Math.round(hue)}, 50%, 50%)`
-        hue += hueStep
-    })
-
-    return colors
 }
 
-function refresh() {
-    var values, maxValue;
-    values = top5Artists[selectedMonth].map(([artist, minutes]) => minutes)
-    artists = top5Artists[selectedMonth].map(([artist, minutes]) => artist)
-    maxValue = Math.max(...values)
+render()
 
-    x = d3.scaleBand()
-        .domain(artists)
-        .range([graphXMargin, graphWidth + graphXMargin])
-        .padding(0.1)
+function bars(svg) {
+    let bar = svg.append("g")
+        .attr("fill-opacity", 0.6)
+        .selectAll("rect");
 
-    y = d3.scaleLinear()
-        .domain([0, maxValue + 50])
-        .range([graphHeight + graphYMargin, graphTopEdge]);
-
-    var bars = dataContainer.selectAll('.bars').data(values);
-
-    bars
-        .enter()
-        .append('rect')
-        .attr('height', 0)
-        .attr('class', 'bars')
-        .attr('x', function (d, i) { return x(top5Artists[selectedMonth][i][0]) })
-        .attr('dx', x.bandwidth())
-        .attr('y', function (d) { return graphHeight; })
-        .attr('value', function (d) { return d; })
-        .attr('artist', function (d, i) { return top5Artists[selectedMonth][i][0]; })
-
-    bars
-        .transition()
-        .duration(1000)
-        .delay(function (d, i) { return i * 50; })
-        .attr('height', function (d) { return graphHeight - y(d); })
-        .attr('x', function (d, i) { return x(top5Artists[selectedMonth][i][0]) })
-        .attr('y', function (d) { return y(d); })
-        .attr('dx', x.bandwidth())
-        .attr('value', function (d) { return d; })
-        .attr('artist', function (d, i) { return top5Artists[selectedMonth][i][0]; })
+    return ([date, data], transition) => bar = bar
+        .data(data.slice(0, n), d => d.name)
+        .join(
+            enter => enter.append("rect")
+                .attr("fill", d => scale(d.name))
+                .attr("height", y.bandwidth())
+                .attr("x", x(0))
+                .attr("y", d => y((prev.get(d) || d).rank))
+                .attr("width", d => x((prev.get(d) || d).value) - x(0)),
+            update => update,
+            exit => exit.transition(transition).remove()
+                .attr("y", d => y((next.get(d) || d).rank))
+                .attr("width", d => x((next.get(d) || d).value) - x(0))
+        )
+        .call(bar => bar.transition(transition)
+            .attr("y", d => y(d.rank))
+            .attr("width", d => x(d.value) - x(0)));
 }
 
-function recalcDimensions() {
-    w = window.innerWidth
-    h = window.innerHeight
+function labels(svg) {
+    let label = svg.append("g")
+        .style("font", "bold 12px var(--sans-serif)")
+        .style("font-variant-numeric", "tabular-nums")
+        .attr("text-anchor", "end")
+        .selectAll("text");
 
-    graphXMargin = w * 0.1
-    graphYMargin = h * 0.1
-    graphWidth = w - (graphXMargin * 2)
-    graphHeight = h - (graphYMargin * 2)
-    barWidth = graphWidth / 5
-    graphTopEdge = sliderMargin
+    return ([date, data], transition) => label = label
+        .data(data.slice(0, n), d => d.name)
+        .join(
+            enter => enter.append("text")
+                .attr("transform", d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
+                .attr("y", y.bandwidth() / 2)
+                .attr("x", -6)
+                .attr("dy", "-0.25em")
+                .text(d => d.name)
+                .call(text => text.append("tspan")
+                    .attr("fill-opacity", 0.7)
+                    .attr("font-weight", "normal")
+                    .attr("x", -6)
+                    .attr("dy", "1.15em")),
+            update => update,
+            exit => exit.transition(transition).remove()
+                .attr("transform", d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
+                .call(g => g.select("tspan").tween("text", d => textTween(d.value, (next.get(d) || d).value)))
+        )
+        .call(bar => bar.transition(transition)
+            .attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
+            .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).value, d.value))));
 }
 
-function resizeSlider() {
-    monthSlider.position(graphXMargin, 20)
-    monthSlider.size(graphWidth - 70)
+function textTween(a, b) {
+    const i = d3.interpolateNumber(a, b);
+    return function (t) {
+        this.textContent = formatNumber(i(t));
+    };
 }
 
-function resizeButton() {
-    button.position(graphWidth + graphXMargin - 50, 20)
+function axis(svg) {
+    const g = svg.append("g")
+        .attr("transform", `translate(0,${marginTop})`);
+
+    const axis = d3.axisTop(x)
+        .ticks(width / 160, tickFormat)
+        .tickSizeOuter(0)
+        .tickSizeInner(-barSize * (n + y.padding()));
+
+    return (_, transition) => {
+        g.transition(transition).call(axis);
+        g.select(".tick:first-of-type text").remove();
+        g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
+        g.select(".domain").remove();
+    };
 }
 
-function sliderSetup() {
-    months = Object.keys(cumulativeListeningData).sort()
+function ticker(svg) {
+    const now = svg.append("text")
+        .style("font", `bold ${barSize}px var(--sans-serif)`)
+        .style("font-variant-numeric", "tabular-nums")
+        .attr("text-anchor", "end")
+        .attr("x", width - 6)
+        .attr("y", marginTop + barSize * (n - 0.45))
+        .attr("dy", "0.32em")
+        .text(formatDate(keyframes[0][0]));
 
-    monthSlider = createSlider(0, months.length - 1, 0, 1)
-    resizeSlider()
-
-    monthSlider.changed(() => {
-        selectedMonth = months[monthSlider.value()]
-    })
-    monthSlider.mouseClicked(() => {
-        isAutoplayOn = false
-    })
+    return ([date], transition) => {
+        transition.end().then(() => now.text(formatDate(date)));
+    };
 }
 
-function buttonSetup() {
-    button = createButton('⏯️')
-    resizeButton()
-    button.mousePressed(() => isAutoplayOn = !isAutoplayOn)
+function rank(value) {
+    const data = Array.from(names, name => ({ name, value: value(name) }));
+    data.sort((a, b) => d3.descending(a.value, b.value));
+    for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
+    return data;
 }
 
-function windowResized() {
-    recalcDimensions()
-    resizeCanvas(w, h)
-    resizeSlider()
-    resizeButton()
-}
-
-function drawMonthText() {
-    const date = new Date(selectedMonth)
-    const monthName = date.toLocaleString('sl', { month: 'long' });
-    const year = date.getFullYear()
-
-    const monthText = monthName + ' ' + year
-
-    textAlign(CENTER)
-    textSize(14)
-    fill(0)
-    text(monthText, w / 2, 50)
-}
-
-function drawFunction() {
-    background(255);
-    drawMonthText(selectedMonth)
-    noStroke();
-
-    var barsHTML = document.getElementsByClassName('bars');
-    var bars = Array.from(barsHTML).map(bar => new p5.Element(bar));
-
-    for (var i = 0; i < bars.length; i++) {
-        var thisbar = bars[i];
-        push();
-        translate(thisbar.attribute('x'), thisbar.attribute('y'));
-        fill(color('#1DB954'));
-        rect(1, 1, thisbar.attribute('dx'), thisbar.attribute('height'));
-        fill('white')
-        text(int(thisbar.attribute('value')), thisbar.attribute('dx') / 2 + 2, 15);
-        text(thisbar.attribute('artist'), thisbar.attribute('dx') / 2 + 2, thisbar.attribute('height') - 10);
-        pop();
-    }
-    stroke('black');
-    strokeWeight(3);
-    line(0, height, width, height);
-    noStroke();
-}
-
-function calculateTop5Artists() {
-    const top5Artists = {}
-
-    for (const month in cumulativeListeningData) {
-        const monthData = cumulativeListeningData[month]
-        const sortedArtists = Object.entries(monthData)
-            .sort(([, a], [, b]) => b - a)
-
-        top5Artists[month] = sortedArtists.slice(0, 5)
-    }
-
-    for (const month in top5Artists) {
-        top5Artists[month] = top5Artists[month].map(([artist, ms]) => [artist, Math.round(ms / 60000)])
-    }
-
-    return top5Artists
-}
-
+formatNumber = d3.format(",d")
